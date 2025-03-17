389cabaf676551fa65d816a1171447c3d1a4e90b
And <ashevcab@google.com>
2025-03-17 23:51:13 +0500

    add timer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 // глобальный обработчик  DOM
 window.addEventListener("DOMContentLoaded", () => {
+  // Tabs
+
   const tabs = document.querySelectorAll(".tabheader__item"),
     tabsContent = document.querySelectorAll(".tabcontent"),
     tabsParent = document.querySelector(".tabheader__items");
@@ -40,6 +42,57 @@ window.addEventListener("DOMContentLoaded", () => {
       });
     }
   });
-
   // end
+
+  //Timer
+
+  const deadline = "2025-03-18";
+
+  function setZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function getTimeRemaining(endTime) {
+    const t = Date.parse(endTime) - Date.parse(new Date()),
+      days = Math.floor(t / (1000 * 60 * 60 * 24)),
+      hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+      minutes = Math.floor((t / (1000 * 60)) % 60),
+      seconds = Math.floor((t / 1000) % 60);
+
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  function setClock(selector, endTime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"),
+      timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const t = getTimeRemaining(endTime);
+
+      days.innerHTML = setZero(t.days);
+      hours.innerHTML = setZero(t.hours);
+      minutes.innerHTML = setZero(t.minutes);
+      seconds.innerHTML = setZero(t.seconds);
+
+      if (t.total <= 0) clearInterval(timeInterval);
+    }
+  }
+
+  setClock(".timer", deadline);
 });
